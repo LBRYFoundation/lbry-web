9b4ab7f935a907028c1ffa2da0a78444f43b7316
Ben van Hartingsveldt <ben.vanhartingsveldt@yocto.com>
2025-11-13 15:53:16 +0100

    Implement basic homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,39 @@
-"use server";
-
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import LBRY from "./LBRY";
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+    const notTags = ["porn","porno","nsfw","mature","xxx","sex","creampie","blowjob","handjob","vagina","boobs","big boobs","big dick","pussy","cumshot","anal","hard fucking","ass","fuck","hentai"];
 
-    console.log('BONK');
+    LBRY.rpc('claim_search',{"page_size":4,"claim_type":["stream","repost","channel"],"no_totals":true,"any_tags":[],"not_tags":notTags,"channel_ids":["80d2590ad04e36fb1d077a9b9e3a8bba76defdf8","b58dfaeab6c70754d792cdd9b56ff59b90aea334"],"not_channel_ids":[],"order_by":["release_time"],"has_source":true,"release_time":">1731193200","include_purchase_receipt":true}).then(json => {
+        json.result.items.forEach((item: object): void => {
+            document.getElementById('row-1').innerHTML += '<div style="border:1px solid red;display:inline-block;max-width:200px;"><img alt="Thumbnail" src="'+(item.value?.thumbnail?.url || item.reposted_claim?.value?.thumbnail?.url)+'" style="height:100px;"><br>'+(item.value?.title || item.reposted_claim?.value?.title)+'</div>';
+        });
+        document.getElementById('row-1').innerHTML += '<br>-------<br>';
+    });
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    LBRY.rpc('claim_search',{"page_size":4,"claim_type":["stream"],"no_totals":true,"any_tags":[],"not_tags":notTags,"channel_ids":[],"not_channel_ids":[],"order_by":["effective_amount"],"has_source":true,"release_time":">1762902000","limit_claims_per_channel":2,"include_purchase_receipt":true}).then(json => {
+        json.result.items.forEach((item: object): void => {
+            document.getElementById('row-2').innerHTML += '<div style="border:1px solid red;display:inline-block;max-width:200px;"><img alt="Thumbnail" src="'+(item.value?.thumbnail?.url || item.reposted_claim?.value?.thumbnail?.url)+'" style="height:100px;"><br>'+(item.value?.title || item.reposted_claim?.value?.title)+'</div>';
+        });
+        document.getElementById('row-2').innerHTML += '<br>-------<br>';
+    });
+
+    LBRY.rpc('claim_search',{"page_size":4,"claim_type":["stream","repost","channel"],"no_totals":true,"any_tags":[],"not_tags":notTags,"channel_ids":["3fda836a92faaceedfe398225fb9b2ee2ed1f01a"],"not_channel_ids":[],"order_by":["release_time"],"has_source":true,"include_purchase_receipt":true}).then(json => {
+        json.result.items.forEach((item: object): void => {
+            document.getElementById('row-3').innerHTML += '<div style="border:1px solid red;display:inline-block;max-width:200px;"><img alt="Thumbnail" src="'+(item.value?.thumbnail?.url || item.reposted_claim?.value?.thumbnail?.url)+'" style="height:100px;"><br>'+(item.value?.title || item.reposted_claim?.value?.title)+'</div>';
+        });
+        document.getElementById('row-3').innerHTML += '<br>-------<br>';
+    });
+
+    return (
+        <>
+            <div id="row-1"></div>
+            <hr/>
+            <div id="row-2"></div>
+            <hr/>
+            <div id="row-3"></div>
+        </>
+    )
 }
 
 export default App
---
+++ src/LBRY.tsx
@@ -0,0 +1,18 @@
+async function rpc(method: string,params?: object): Promise<any>{
+    return await (await fetch(import.meta.env.VITE_DEFAULT_DAEMON, {
+        method: 'POST',
+        // headers: {
+        //     'Content-Type': 'application/json',
+        // },
+        body: JSON.stringify({
+            jsonrpc: '2.0',
+            method: method,
+            params: params,
+            id: Math.ceil(Math.random()*65536),
+        }),
+    })).json();
+}
+
+export default {
+    rpc,
+};
