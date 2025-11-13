c88fe9118aa91aa3815f4d23833ccc955de42c79
Ben van Hartingsveldt <ben.vanhartingsveldt@yocto.com>
2025-11-13 16:18:17 +0100

    Fix building

--- src/LBRY.tsx
+++ src/LBRY.tsx
@@ -1,4 +1,4 @@
-async function rpc(method: string,params?: object): Promise<any>{
+async function rpc(method: string,params?: object): Promise<object>{
     return await (await fetch(import.meta.env.VITE_DEFAULT_DAEMON, {
         method: 'POST',
         // headers: {
