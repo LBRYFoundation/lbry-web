ce1f25d348778f60fb80ef2b8c9af6fd662d1492
Ben van Hartingsveldt <ben.vanhartingsveldt@yocto.com>
2025-11-30 01:28:28 +0100

    Fix ESLint

--- eslint.config.js
+++ eslint.config.js
@@ -1,17 +1,15 @@
-import js from "@eslint/js";
+import eslint from "@eslint/js";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import { defineConfig, globalIgnores } from "eslint/config";
-import tseslint from "typescript-eslint";
 
 export default defineConfig([
   globalIgnores(["dist"]),
   {
     extends: [
-      js.configs.recommended,
+      eslint.configs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
-      tseslint.configs.recommended,
     ],
   },
 ]);
